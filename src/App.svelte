164b89955f5257d9d7b570e3ac7bc4f8caa91c20
OscarClemente <oscar.clemente@gmail.com>
2021-05-01 14:28:46 +0200

    Added general Timers component

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,9 @@
 <script>
-	export let name;
+	import Timers from './components/Timers.svelte'
+	import {initClient} from "@urql/svelte"
+
+	let timers = [{id: 1, title: "Example timer 1"},
+								{id: 2, title: "Example timer 2"},]
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Timers {timers}/>
---
+++ src/components/Timers.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let timers = []
+</script>
+
+<div class="timerapp stack-large">
+  <!-- Timers -->
+  <ul role="list" class="timer-list stack-large" aria-labelledby="list-heading">
+    {#each timers as timer (timer.id)}
+      <li class="timer">
+        {timer.id} - {timer.title}
+      </li>
+    {:else}
+      <li>
+        No timers saved!
+    {/each}
+  <ul>
+</div>
